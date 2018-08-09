7c12a059399b87867c8da5873bbf9b604b7dbdf5
Ales Krejci <aleskrejcicz@gmail.com>
2018-08-09 05:36:06 +0200

    fix parsed

--- parse_url/parse_url.py
+++ parse_url/parse_url.py
@@ -1,103 +1,40 @@
 import json
-import re
-
-
-class Parse:
-    def __init__(self):
-        self.url_pattern = {
-            'scheme': None,
-            'user': None,
-            'pass': None,
-            'host': None,
-            'port': None,
-            'path': None,
-            'query': None,
-            'fragment': None
-        }
-
-    def http_and_https(self, data):
-        return self.url_pattern
-
-    def ftp_and_ftps(self, data):
-        return self.url_pattern
-
-    def ssh(self, data):
-        return self.url_pattern
-
-    def git(self, data):
-        return self.url_pattern
-
-    def file(self, data):
-        return self.url_pattern
-
-    def rsync(self, data):
-        return self.url_pattern
-
-    def mailto(self, data):
-        return self.url_pattern
-
-    def telnet(self, data):
-        return self.url_pattern
-
-    def nntp(self, data):
-        return self.url_pattern
-
-
 from urllib.parse import urlparse
-from urllib.parse import urlsplit
 
 
-# url = 'https://user:pass@NetLoc.com:80/path;parxxam?query=argument#fragment'
-# parsed = urlparse(url)
-# print('scheme  :', parsed.scheme)
-# print('netloc  :', parsed.netloc)
-# print('path    :', parsed.path)
-# print('params  :', parsed.params)
-# print('query   :', parsed.query)
-# print('fragment:', parsed.fragment)
-# print('username:', parsed.username)
-# print('password:', parsed.password)
-# print('hostname:', parsed.hostname)
-# print('port    :', parsed.port)
-# print("---------------")
-#
-# parsed = urlsplit(url)
-# print(parsed)
-#
-
-def parse_url(url: str):
-    c = Parse()
-    scheme_position = url.find(':')
-    print(url, scheme_position)
-    if scheme_position > 0:
-        scheme_word = url[:scheme_position]
-        if re.match(r'^(http|https)$', scheme_word):
-            return c.http_and_https(url)
-        elif re.match(r'^(ftp|ftps)$', scheme_word):
-            return c.ftp_and_ftps(url)
-        elif re.match(r'^(ssh)$', scheme_word):
-            return c.ssh(url)
-        elif re.match(r'^(git)$', scheme_word):
-            return c.git(url)
-        elif re.match(r'^(file)$', scheme_word):
-            return c.file(url)
-        elif re.match(r'^(rsync)$', scheme_word):
-            return c.rsync(url)
-        elif re.match(r'^(mailto)$', scheme_word):
-            return c.mailto(url)
-        elif re.match(r'^(telnet)$', scheme_word):
-            return c.telnet(url)
-        elif re.match(r'^(nntp)$', scheme_word):
-            return c.nntp(url)
-    return
+root_dir = 'example_data'
+filename = 'url.http.json'
 
 
-ahoj = []
-with open('example_data/input/url.http.json') as data_file:
-    urls = json.load(data_file)
-    for url in urls.keys():
-        ahoj.append(url)
+def extract(root_dir, filename):
+    parsed_data = {}
+    with open('%s/input/%s' % (root_dir, filename)) as f:
+        for url in json.loads(f.read()):
+            parsed = urlparse(url)
+            try:
+                port = parsed.port
+            except Exception as e:
+                port = None
+
+            parsed_data[url] = {
+                'scheme': parsed.scheme,
+                'netloc': parsed.netloc,
+                'path': parsed.path,
+                'params': parsed.params,
+                'query': parsed.query,
+                'fragment': parsed.fragment,
+                'username': parsed.username,
+                'password': parsed.password,
+                'hostname': parsed.hostname,
+                'port': port
+            }
+    return parsed_data
 
 
-with open('example_data/output/python/url.http.json', 'a') as data_file:
-    data_file.write(json.dumps(ahoj))
+def save(root_dir, filename, parsed_data):
+    with open('%s/output/python/%s' % (root_dir, filename), 'w') as f:
+        f.write(json.dumps(parsed_data))
+
+
+parsed_data = extract(root_dir, filename)
+save(root_dir, filename, parsed_data)
---
+++ parse_url/parse_url_old.py
@@ -0,0 +1,103 @@
+import json
+import re
+
+
+class Parse:
+    def __init__(self):
+        self.url_pattern = {
+            'scheme': None,
+            'user': None,
+            'pass': None,
+            'host': None,
+            'port': None,
+            'path': None,
+            'query': None,
+            'fragment': None
+        }
+
+    def http_and_https(self, data):
+        return self.url_pattern
+
+    def ftp_and_ftps(self, data):
+        return self.url_pattern
+
+    def ssh(self, data):
+        return self.url_pattern
+
+    def git(self, data):
+        return self.url_pattern
+
+    def file(self, data):
+        return self.url_pattern
+
+    def rsync(self, data):
+        return self.url_pattern
+
+    def mailto(self, data):
+        return self.url_pattern
+
+    def telnet(self, data):
+        return self.url_pattern
+
+    def nntp(self, data):
+        return self.url_pattern
+
+
+from urllib.parse import urlparse
+from urllib.parse import urlsplit
+
+
+# url = 'https://user:pass@NetLoc.com:80/path;parxxam?query=argument#fragment'
+# parsed = urlparse(url)
+# print('scheme  :', parsed.scheme)
+# print('netloc  :', parsed.netloc)
+# print('path    :', parsed.path)
+# print('params  :', parsed.params)
+# print('query   :', parsed.query)
+# print('fragment:', parsed.fragment)
+# print('username:', parsed.username)
+# print('password:', parsed.password)
+# print('hostname:', parsed.hostname)
+# print('port    :', parsed.port)
+# print("---------------")
+#
+# parsed = urlsplit(url)
+# print(parsed)
+#
+
+def parse_url(url: str):
+    c = Parse()
+    scheme_position = url.find(':')
+    print(url, scheme_position)
+    if scheme_position > 0:
+        scheme_word = url[:scheme_position]
+        if re.match(r'^(http|https)$', scheme_word):
+            return c.http_and_https(url)
+        elif re.match(r'^(ftp|ftps)$', scheme_word):
+            return c.ftp_and_ftps(url)
+        elif re.match(r'^(ssh)$', scheme_word):
+            return c.ssh(url)
+        elif re.match(r'^(git)$', scheme_word):
+            return c.git(url)
+        elif re.match(r'^(file)$', scheme_word):
+            return c.file(url)
+        elif re.match(r'^(rsync)$', scheme_word):
+            return c.rsync(url)
+        elif re.match(r'^(mailto)$', scheme_word):
+            return c.mailto(url)
+        elif re.match(r'^(telnet)$', scheme_word):
+            return c.telnet(url)
+        elif re.match(r'^(nntp)$', scheme_word):
+            return c.nntp(url)
+    return
+
+
+ahoj = []
+with open('example_data/input/url.http.json') as data_file:
+    urls = json.load(data_file)
+    for url in urls.keys():
+        ahoj.append(url)
+
+
+with open('example_data/output/python/url.http.json', 'a') as data_file:
+    data_file.write(json.dumps(ahoj))
