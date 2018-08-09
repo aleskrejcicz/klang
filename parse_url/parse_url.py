f4794f3221fed190361223c1fbfe29bc418c15bb
Ales Krejci <aleskrejcicz@gmail.com>
2018-08-09 04:33:39 +0200

    estetika

--- parse_url/parse_url.py
+++ parse_url/parse_url.py
@@ -43,6 +43,28 @@ class Parse:
         return self.url_pattern
 
 
+from urllib.parse import urlparse
+from urllib.parse import urlsplit
+
+
+url = 'https://user:pass@NetLoc.com:80/path;parxxam?query=argument#fragment'
+parsed = urlparse(url)
+print('scheme  :', parsed.scheme)
+print('netloc  :', parsed.netloc)
+print('path    :', parsed.path)
+print('params  :', parsed.params)
+print('query   :', parsed.query)
+print('fragment:', parsed.fragment)
+print('username:', parsed.username)
+print('password:', parsed.password)
+print('hostname:', parsed.hostname)
+print('port    :', parsed.port)
+print("---------------")
+
+parsed = urlsplit(url)
+print(parsed)
+
+
 def parse_url(url: str):
     c = Parse()
     scheme_position = url.find(':')
