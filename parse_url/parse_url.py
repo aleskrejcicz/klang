8fabb6847d5519ca51e35d57f420f570822ff71c
Ales Krejci <aleskrejcicz@gmail.com>
2018-08-12 01:55:08 +0200

    merge and update

--- parse_url/parse_url.py
+++ parse_url/parse_url.py
@@ -1,3 +1,4 @@
+import re
 import json
 from urllib.parse import urlparse
 
@@ -7,26 +8,52 @@ input_dir = '%s/input' % root_dir
 output_dir = '%s/output/python' % root_dir
 
 
-def ext_data(fwp):
-    data = {}
-    with open(fwp) as f:
-        for url in json.loads(f.read()):
-            parsed = urlparse(url)
-            data[url] = {
-                'data': {
-                    'scheme': parsed.scheme,
-                    'netloc': parsed.netloc,
-                    'path': parsed.path,
-                    'params': parsed.params,
-                    'query': parsed.query,
-                    'fragment': parsed.fragment,
-                    'username': parsed.username,
-                    'password': parsed.password,
-                    'hostname': parsed.hostname,
-                    'port': parsed.port
-                }
-            }
-    return data
+def extract_data(url: str):
+    parsed = urlparse(url)
+    return {
+        'scheme': parsed.scheme,
+        'netloc': parsed.netloc,
+        'path': parsed.path,
+        'params': parsed.params,
+        'query': parsed.query,
+        'fragment': parsed.fragment,
+        'username': parsed.username,
+        'password': parsed.password,
+        'hostname': parsed.hostname,
+        'port': parsed.port
+    }
+
+
+def parse_url(url: str):
+    scheme_position = url.find(':')
+    if scheme_position > 0:
+        # 7. Application layer:
+        # https://en.wikipedia.org/wiki/Application_layer
+        # -----------------
+        # Services:
+        # BitTorrent • DNS • BOOTP
+        # DHCP • FTP • HTTP • HTTPS • IMAP • IRC • Ident • NNTP • NFS • NTP • POP3
+        # RTP • SIP • SMB • SMTP • SNMP • SSH • STUN • Telnet • XMPP
+        scheme_word = url[:scheme_position]
+        if re.match(r'^(http|https)$', scheme_word):
+            return extract_data(url)
+        elif re.match(r'^(ftp|ftps)$', scheme_word):
+            pass
+        elif re.match(r'^(ssh)$', scheme_word):
+            pass
+        elif re.match(r'^(git)$', scheme_word):
+            pass
+        elif re.match(r'^(file)$', scheme_word):
+            pass
+        elif re.match(r'^(rsync)$', scheme_word):
+            pass
+        elif re.match(r'^(mailto)$', scheme_word):
+            pass
+        elif re.match(r'^(telnet)$', scheme_word):
+            pass
+        elif re.match(r'^(nntp)$', scheme_word):
+            pass
+    return None
 
 
 def save_to_file(fwp, parsed_data):
@@ -35,7 +62,14 @@ def save_to_file(fwp, parsed_data):
         f.write(json_data)
 
 
-for f in ['http.json', 'https.json']:
-    fwp_input = '%s/%s' % (input_dir, f)
-    fwp_output = '%s/%s' % (output_dir, f)
-    save_to_file(fwp_output, ext_data(fwp_input))
+if __name__ == '__main__':
+    for f in ['http.json', 'https.json']:
+        fwp_input = '%s/%s' % (input_dir, f)
+        fwp_output = '%s/%s' % (output_dir, f)
+
+        data = {}
+        with open(fwp_input) as f:
+            for url in json.loads(f.read()):
+                data[url] = {'data': parse_url(url)}
+
+        save_to_file(fwp_output, data)
